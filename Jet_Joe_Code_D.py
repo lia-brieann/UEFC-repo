d05d6a6df2426af19a4d152036e4c6a5f47b3db7
lia-brieann <aby@mit.edu>
2026-05-01 14:30:25 -0400

    worked on drone

--- Jet_Joe_Code_D.py
+++ Jet_Joe_Code_D.py
@@ -8,30 +8,42 @@ import math as math
 from scipy.optimize import fsolve
 
 LHV = 44.5 * 10**6      # [J/kg] fuel heating value
-g = 9.81 # m/s
+g = -9.81 # m/s
 LoD = 10 # cruise L/D
 ToW_TO = 0.34 # takeoff T/W
-ToW__C = 0.1 # cruise T/W
+ToW_C = 0.1 # cruise T/W
 W0 = 32 # empty aircraft weight (lbs)
 fmax = 1.0 # fuel capacity (gal)
 rho_f = 6.843 # fuel density (lbs/gal)
-Wf = fmax * rho_f # fuel weight (lbs)
-Wtotal = (W0 + Wf)*0.453592 # total takeoff weight (kg)
-alt = 2000 #ft
+Wf0 = fmax * rho_f # fuel weight (lbs)
+Wtotal = (W0 + Wf0)*0.453592 # total takeoff weight (kg)
+W_TO = Wtotal
+alt = 2000*0.3048 #ft --> m
 
-def drone_range(mdot_f, t, mdot_j):
+def drone_range(mdot_f, SFC):
 
-    # need to calculate overall efficiency based on flight speed
+    # # first we find the time to reach altitude
+    # a = g*(ToW_TO-1)
+    # t_TO = np.sqrt(2*alt/a) # takeoff time
+    # # print(f't_TO = {t_TO}')
+
+    # # then we use t_TO to find the change in weight
+    # # we'll assume that the drone descends at the same rate as it ascends, so we double the fuel loss
+    # W = W_TO - 2*mdot_f*(-g)*t_TO
+    # print(f'initial weight = {W_TO}, weight minus takeoff and landing fuel = {W}')
+
+    # use breguet range equation to evaluate range in cruise
+    # calculate overall efficiency based on flight speed
     c = 100 * 0.51444 # knots --> m/s
-    T = t*mdot_j
-    mu_0 = (T*c)/(mdot_f*LHV) # overall efficiency
-    R = (LHV/g)*mu_0*(LoD)*np.log(W0/Wtotal) # m
+    T = mdot_f/SFC
 
+    mu_0 = (T*c)/(mdot_f*LHV) # overall efficiency
+    R = -(LHV/g)*mu_0*(LoD)*np.log(W0/Wtotal) # m
     print(f'Range = {R} (m)\n      = {R/1000} (km)\n      = {R*0.000621371} (mi)')
 
     return R
 
-drone_range(0.001290232,4.526,0.2997)
+# drone_range(0.001290232,4.526,0.2997)
 
 
 def static_thrust():
